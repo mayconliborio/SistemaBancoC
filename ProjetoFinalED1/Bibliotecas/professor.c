4c6cb2d0af597f43354f6cf8720fd20720c44dea
Adam Augusto <adamleme@hotmail.com>
2019-06-28 21:10:55 -0300

    v0.6.7

--- ProjetoFinalED1/Bibliotecas/professor.c
+++ ProjetoFinalED1/Bibliotecas/professor.c
@@ -1,18 +1,105 @@
 //Professor
 
-void inserirNotas(){
+void inserirNotas(aluno *oAluno, float aNota, turma *aTurma, int ordemNota){
+    int j;
+    int n;
+    for(j=0;j<45;j++){
+        if((aTurma->alunos[j]!=NULL){
+            n=strcasecmp((aTurma->alunos[j])->ra,oAluno->ra);
+            if(n==0){
+                    tipoAproveitamento *aux=oAluno->aproveitamento;
+                     
+                    while(aux!=NULL){
+                        if((aux->turma)->codigo==aTurma->codigo)
+                        {
+                            aux->notas[ordemNota]=aNota;
+                            return;
+                        }
+                        aux=aux->prox;
+                    }            
+            }
+        }
+    }
+    printf("\nAluno não cadastrado nesta turma!\n");
+    return;
+}
+
+void alterarNotas(aluno *oAluno, float novaNota,turma *aTurma,int ordemNota){
+    int j;
+    int n;
+    for(j=0;j<45;j++){
+        if((aTurma->alunos[j]!=NULL){
+            n=strcasecmp((aTurma->alunos[j])->ra,oAluno->ra);
+            if(n==0){
+                    tipoAproveitamento *aux=oAluno->aproveitamento;
+                     
+                    while(aux!=NULL){
+                        if((aux->turma)->codigo==aTurma->codigo)
+                        {   if(aux->notas[ordemNota]!=-1){
+                                aux->notas[ordemNota]=aNota;
+                                return;
+                            }
+                            printf("\nA nota não existe para ser alterada!\n");
+                            return;
+                        }
+                        aux=aux->prox;
+                    }            
+            }
+        }
+    }
+    printf("\nAluno não cadastrado nesta turma!\n");
+    return;
 
 }
 
-void alterarNotas(){
+void inserirFrequencia(aluno *oAluno,turma *aTurma){
+    int j;
+    int n;
+    for(j=0;j<45;j++){
+        if((aTurma->alunos[j]!=NULL){
+            n=strcasecmp((aTurma->alunos[j])->ra,oAluno->ra);
+            if(n==0){
+                    tipoAproveitamento *aux=oAluno->aproveitamento;
+                     
+                    while(aux!=NULL){
+                        if((aux->turma)->codigo==aTurma->codigo)
+                        {
+                            (aux->frequencia)++;
+                            return;
+                        }
+                        aux=aux->prox;
+                    }            
+            }
+        }
+    }
+    printf("\nAluno não cadastrado nesta turma!\n");
+    return;
 
 }
 
-void inserirFrequencia(){
+void alterarFrequencia(aluno *oAluno,turma *aTurma, float novaFrenquencia){
+    int j;
+    int n;
+    for(j=0;j<45;j++){
+        if((aTurma->alunos[j]!=NULL){
+            n=strcasecmp((aTurma->alunos[j])->ra,oAluno->ra);
+            if(n==0){
+                    tipoAproveitamento *aux=oAluno->aproveitamento;
+                     
+                    while(aux!=NULL){
+                        if((aux->turma)->codigo==aTurma->codigo)
+                        {
+                            (aux->frequencia)=novaFrequencia;
+                            return;
+                        }
+                        aux=aux->prox;
+                    }            
+            }
+        }
+    }
+    printf("\nAluno não cadastrado nesta turma!\n");
+    return;
 
-}
-
-void alterarFrequencia(){
 
 }
 
